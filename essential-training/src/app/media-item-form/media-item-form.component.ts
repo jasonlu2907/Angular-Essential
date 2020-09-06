a393106274c94d08e51028d7a71e9724f0a819c0
jasonlu2907@gmail.com <jasonlu2907@gmail.com>
2020-09-06 15:57:18 -0500

    Built-on+Custom Validator

--- essential-training/src/app/media-item-form/media-item-form.component.ts
+++ essential-training/src/app/media-item-form/media-item-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-media-item-form',
   templateUrl: './media-item-form.component.html',
@@ -10,15 +9,34 @@ export class MediaItemFormComponent implements OnInit {
   form: FormGroup;
 
   constructor() {
+  }
+
+  ngOnInit() {
     this.form = new FormGroup({
       medium: new FormControl('Movies'),
-      name: new FormControl(''),
+      // Built-on Validator
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
       category: new FormControl(''),
-      year: new FormControl('')
+      year: new FormControl('', this.yearValidator)
     });
   }
 
-  ngOnInit(): void {
+  // Custom Validator
+  yearValidator(control: FormControl) {
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+    const year = parseInt(control.value, 10);
+    const minYear = 1900;
+    const maxYear = 2100;
+    if (year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return { year: true };
+    }
   }
 
   onSubmit(mediaItem: any) {
