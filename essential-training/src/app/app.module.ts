e7f867024fb08a5d2907349ff183f718066cb22f
jasonlu2907@gmail.com <jasonlu2907@gmail.com>
2020-09-05 23:20:39 -0500

    Pipes builtin+custom, Directives and Pipes completed

--- essential-training/src/app/app.module.ts
+++ essential-training/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
 import { MediaItemListComponent } from './media-item-list/media-item-list.component';
 import { FavoriteDirective } from './directives/favorite.directive';
+import { CategoryListPipe } from './pipes/category-list.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
-    FavoriteDirective
+    FavoriteDirective,
+    CategoryListPipe
   ],
   imports: [
     BrowserModule
