4ac761e4d384c2d5be43327068f1f86ef77a14b5
jasonlu2907@gmail.com <jasonlu2907@gmail.com>
2020-09-06 16:13:15 -0500

    Error Handling

--- essential-training/src/app/media-item-form/media-item-form.component.ts
+++ essential-training/src/app/media-item-form/media-item-form.component.ts
@@ -35,7 +35,11 @@ export class MediaItemFormComponent implements OnInit {
     if (year >= minYear && year <= maxYear) {
       return null;
     } else {
-      return { year: true };
+      return { year: {
+          min: minYear,
+          max: maxYear
+        } 
+      };
     }
   }
 
